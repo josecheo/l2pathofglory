18ff5a34af5f735582b1098002194efdcf18416c
joseche0 <joseche0.ja@gmail.com>
2026-01-07 18:37:41 -0400

    add vote

--- src/components/SocialFloating.tsx
+++ src/components/SocialFloating.tsx
@@ -5,6 +5,9 @@ import { faDiscord, faFacebook, faInstagram, faYoutube, faTelegram } from '@fort
 const SocialFloating = () => {
   return (
     <div className="fixed top-1/2 right-4 transform -translate-y-1/2 flex flex-col gap-3 z-50">
+      <a href="https://l2top.co/vote/server/Pathofglory" target="_blank" rel="noopener noreferrer" className="w-10 h-10 bg-gradient-to-r from-yellow-600 to-amber-700 hover:from-yellow-500 hover:to-amber-600 rounded-full flex items-center justify-center">
+        <img src="https://l2top.co/img/banners/l2topco_mini2.png" alt="Vote for Pathofglory in L2Top.CO" />      </a>
+
       <a href="https://discord.com" target="_blank" rel="noopener noreferrer" className="w-10 h-10 bg-gradient-to-r from-yellow-600 to-amber-700 hover:from-yellow-500 hover:to-amber-600 rounded-full flex items-center justify-center">
         <FontAwesomeIcon icon={faDiscord} className="text-white text-xl" />
       </a>
